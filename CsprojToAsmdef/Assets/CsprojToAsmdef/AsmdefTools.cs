8822676a22f63a5b4caea9e951eca12883af1982
Kurai András <k.andras9642@gmail.com>
2021-02-19 22:02:36 +0100

    get most asmdef references from csproj

--- CsprojToAsmdef/Assets/CsprojToAsmdef/AsmdefTools.cs
+++ CsprojToAsmdef/Assets/CsprojToAsmdef/AsmdefTools.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -14,7 +16,30 @@ namespace CsprojToAsmdef
                 var projectName = Path.GetFileNameWithoutExtension(filePath);
                 var asmdefPath = Path.Combine(Path.GetDirectoryName(filePath)!, projectName + ".asmdef");
 
-                var asmdef = Asmdef.Default(projectName);
+                var csprojLines = File.ReadAllLines(filePath);
+
+                var includePlatforms = csprojLines.GetCollectionProperty("IncludePlatforms");
+                var excludePlatforms = csprojLines.GetCollectionProperty("ExcludePlatforms");
+                var allowUnsafeCode = csprojLines.GetBoolProperty("AllowUnsafeBlocks", false);
+                var autoReferenced = csprojLines.GetBoolProperty("AutoReferenced", true);
+                var defineConstraints = csprojLines.GetCollectionProperty("DefineConstraints");
+                var versionDefines = csprojLines.GetCollectionProperty("VersionDefines");
+                var noEngineReferences = csprojLines.GetBoolProperty("NoEngineReferences", false);
+
+                var asmdef = new Asmdef
+                {
+                    name = projectName,
+                    references = new string[0],
+                    includePlatforms = includePlatforms,
+                    excludePlatforms = excludePlatforms,
+                    allowUnsafeCode = allowUnsafeCode,
+                    overrideReferences = false,
+                    precompiledReferences = new string[0],
+                    autoReferenced = autoReferenced,
+                    defineConstraints = defineConstraints,
+                    versionDefines = versionDefines,
+                    noEngineReferences = noEngineReferences,
+                };
 
                 var json = EditorJsonUtility.ToJson(asmdef, true);
 
@@ -24,8 +49,28 @@ namespace CsprojToAsmdef
             }
         }
 
+        private static string[] GetCollectionProperty(this IEnumerable<string> csprojLines, string propertyName) =>
+            csprojLines
+                .SingleOrDefault(l => l.Trim().StartsWith($"<{propertyName}>"))
+                ?.Trim()
+                .Replace($"<{propertyName}>", string.Empty)
+                .Replace($"</{propertyName}>", string.Empty)
+                .Split(';')
+                .ToArray();
+
+        private static bool GetBoolProperty(this IEnumerable<string> csprojLines, string propertyName, bool defaultValue) =>
+            csprojLines
+                .SingleOrDefault(l => l.Trim().StartsWith($"<{propertyName}>"))
+                ?.Trim()
+                .Replace($"<{propertyName}>", string.Empty)
+                .Replace($"</{propertyName}>", string.Empty)
+                .ToBool() ?? defaultValue;
+
+        private static bool ToBool(this string text) => bool.Parse(text);
+
         [SuppressMessage("ReSharper", "InconsistentNaming")]
         [SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
+        [SuppressMessage("ReSharper", "NotAccessedField.Local")]
         [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "False positive")]
         private sealed class Asmdef
         {
@@ -40,21 +85,6 @@ namespace CsprojToAsmdef
             public string[] defineConstraints;
             public string[] versionDefines;
             public bool noEngineReferences;
-
-            public static Asmdef Default(string name) => new Asmdef
-            {
-                name = name,
-                references = new string[0],
-                includePlatforms = new string[0],
-                excludePlatforms = new string[0],
-                allowUnsafeCode = false,
-                overrideReferences = false,
-                precompiledReferences = new string[0],
-                autoReferenced = true,
-                defineConstraints = new string[0],
-                versionDefines = new string[0],
-                noEngineReferences = false
-            };
         }
     }
 }
