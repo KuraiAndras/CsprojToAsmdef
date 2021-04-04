c14477bfbfd5b4d3f7e2eb0fb2cd9359c51d6ccb
Kurai András <k.andras9642@gmail.com>
2021-04-04 10:37:42 +0200

    add dotnet tool

---
+++ CsprojToAsmdef.Tool/GetProjectProperties.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace CsprojToAsmdef.Tool
+{
+    [Command]
+    public sealed class GetProjectProperties : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            await console.Output.WriteLineAsync("Hello");
+        }
+    }
+}
---
+++ CsprojToAsmdef.Tool/Program.cs
@@ -0,0 +1,39 @@
+﻿using CliFx;
+using Microsoft.Extensions.DependencyInjection;
+using MoreLinq.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsprojToAsmdef.Tool
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args) =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .UseTypeActivator(CreateTypeActivator())
+                .Build()
+                .RunAsync(args);
+
+        private static Func<Type, object> CreateTypeActivator() => new ServiceCollection()
+            .AddCommands()
+            .AddServices()
+            .BuildServiceProvider()
+            .GetService;
+
+        private static IServiceCollection AddCommands(this IServiceCollection services)
+        {
+            typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => !t.IsInterface && !t.IsAbstract && typeof(ICommand).IsAssignableFrom(t))
+                .ForEach(t => services.AddTransient(t));
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services) =>
+            services;
+    }
+}
