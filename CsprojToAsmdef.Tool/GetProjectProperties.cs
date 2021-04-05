d449143e2c21201f420eb542009201d8020aac11
Kurai András <k.andras9642@gmail.com>
2021-04-05 20:25:00 +0200

    write asmdef

--- CsprojToAsmdef.Tool/GetProjectProperties.cs
+++ CsprojToAsmdef.Tool/GetProjectProperties.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Immutable;
-using CliFx;
+﻿using CliFx;
 using CliFx.Attributes;
 using CliFx.Exceptions;
 using CliFx.Infrastructure;
-using CsprojToAsmdef.Tool.Extensions;
+using CliWrap;
 using Microsoft.Build.Evaluation;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using CliWrap;
-using MoreLinq;
 
 namespace CsprojToAsmdef.Tool
 {
-    [Command]
+    [Command(nameof(GetProjectProperties))]
     public sealed class GetProjectProperties : ICommand
     {
         private readonly IDotNetTooling _dotNet;
@@ -34,9 +35,65 @@ namespace CsprojToAsmdef.Tool
 
             var project = new Project(ProjectPath);
 
-            await project
-                .AllEvaluatedProperties
-                .ForEachAsync(async p => await console.Output.WriteLineAsync($"Name: {p.Name} Evaluated Value: {p.EvaluatedValue}"));
+            var properties = project.AllEvaluatedProperties;
+
+            var name = Path.GetFileNameWithoutExtension(ProjectPath);
+            var references = ImmutableArray<string>.Empty;
+            var includePlatforms = GetCollectionProperty(properties, nameof(Asmdef.IncludePlatforms));
+            var excludePlatforms = GetCollectionProperty(properties, nameof(Asmdef.ExcludePlatforms));
+            var allowUnsafeCode = GetBoolProperty(properties, "AllowUnsafeBlocks", false);
+            var overrideReferences = false;
+            var precompiledReferences = ImmutableArray<string>.Empty;
+            var autoReferenced = GetBoolProperty(properties, nameof(Asmdef.AutoReferenced), true);
+            var defineConstraints = GetCollectionProperty(properties, nameof(Asmdef.DefineConstraints));
+            var versionDefines = GetCollectionProperty(properties, nameof(Asmdef.VersionDefines));
+            var noEngineReferences = GetBoolProperty(properties, nameof(Asmdef.NoEngineReferences), false);
+
+            var asmdef = new Asmdef(
+                name,
+                references,
+                includePlatforms,
+                excludePlatforms,
+                allowUnsafeCode,
+                overrideReferences,
+                precompiledReferences,
+                autoReferenced,
+                defineConstraints,
+                versionDefines,
+                noEngineReferences);
+
+            var asmdefPath = Path.Combine(Path.GetDirectoryName(ProjectPath)!, name + ".asmdef");
+
+            var json = JsonSerializer
+                .Serialize(
+                    asmdef,
+                    new JsonSerializerOptions(JsonSerializerDefaults.General)
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        WriteIndented = true,
+                    })
+                .Replace("  ", "    ");
+
+            await File.WriteAllTextAsync(asmdefPath, json);
+        }
+
+        private static ImmutableArray<string> GetCollectionProperty(IEnumerable<ProjectProperty> properties, string propertyName) =>
+            (properties
+                .SingleOrDefault(p => p.Name == propertyName)
+                ?.EvaluatedValue)
+            ?.Split(';')
+            .ToImmutableArray()
+            ?? ImmutableArray<string>.Empty;
+
+        private static bool GetBoolProperty(IEnumerable<ProjectProperty> properties, string propertyName, bool defaultValue)
+        {
+            var propertyValueString = properties
+                .SingleOrDefault(p => p.Name == propertyName)
+                ?.EvaluatedValue;
+
+            return bool.TryParse(propertyValueString, out var propertyValue)
+                ? propertyValue
+                : defaultValue;
         }
 
         public interface IDotNetTooling
@@ -65,9 +122,54 @@ namespace CsprojToAsmdef.Tool
                     ))
                     .ToImmutableArray();
 
-                var sdkPath = sdkPaths.MaxBy(p => p.version).First();
-                Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", sdkPath.path);
+                var sdkPath = sdkPaths
+                    .MaxBy(p => p.version)
+                    .First()
+                    .path;
+
+                Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", sdkPath);
             }
+        }
+
+        public sealed class Asmdef
+        {
+            public Asmdef(
+                string name,
+                ImmutableArray<string> references,
+                ImmutableArray<string> includePlatforms,
+                ImmutableArray<string> excludePlatforms,
+                bool allowUnsafeCode,
+                bool overrideReferences,
+                ImmutableArray<string> precompiledReferences,
+                bool autoReferenced,
+                ImmutableArray<string> defineConstraints,
+                ImmutableArray<string> versionDefines,
+                bool noEngineReferences)
+            {
+                Name = name;
+                References = references;
+                IncludePlatforms = includePlatforms;
+                ExcludePlatforms = excludePlatforms;
+                AllowUnsafeCode = allowUnsafeCode;
+                OverrideReferences = overrideReferences;
+                PrecompiledReferences = precompiledReferences;
+                AutoReferenced = autoReferenced;
+                DefineConstraints = defineConstraints;
+                VersionDefines = versionDefines;
+                NoEngineReferences = noEngineReferences;
+            }
+
+            public string Name { get; }
+            public ImmutableArray<string> References { get; }
+            public ImmutableArray<string> IncludePlatforms { get; }
+            public ImmutableArray<string> ExcludePlatforms { get; }
+            public bool AllowUnsafeCode { get; }
+            public bool OverrideReferences { get; }
+            public ImmutableArray<string> PrecompiledReferences { get; }
+            public bool AutoReferenced { get; }
+            public ImmutableArray<string> DefineConstraints { get; }
+            public ImmutableArray<string> VersionDefines { get; }
+            public bool NoEngineReferences { get; }
         }
     }
 }
