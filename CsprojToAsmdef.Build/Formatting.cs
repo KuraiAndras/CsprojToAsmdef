99d0c5cee87f5186c934c6611bf1830cece637a5
Kurai András <k.andras9642@gmail.com>
2021-04-10 12:24:42 +0200

    run dotnet format through nuke

---
+++ CsprojToAsmdef.Build/Formatting.cs
@@ -0,0 +1,14 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tooling;
+
+partial class Build
+{
+    [PackageExecutable("dotnet-format", "dotnet-format.dll")]
+    readonly Tool DotNetFormat = default!;
+
+    Target CheckFormat => _ => _
+        .Executes(() => DotNetFormat("--check --verbosity diagnostic"));
+
+    Target Format => _ => _
+        .Executes(() => DotNetFormat("--verbosity diagnostic"));
+}
