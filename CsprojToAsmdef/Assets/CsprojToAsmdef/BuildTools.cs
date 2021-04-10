3980ddbf05793b7fb1a9a6038948dc1153bc0102
Kurai András <k.andras9642@gmail.com>
2021-04-10 12:34:29 +0200

    run format

--- CsprojToAsmdef/Assets/CsprojToAsmdef/BuildTools.cs
+++ CsprojToAsmdef/Assets/CsprojToAsmdef/BuildTools.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
--- CsprojToAsmdef/Assets/CsprojToAsmdef/MethodLocker.cs
+++ CsprojToAsmdef/Assets/CsprojToAsmdef/MethodLocker.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
