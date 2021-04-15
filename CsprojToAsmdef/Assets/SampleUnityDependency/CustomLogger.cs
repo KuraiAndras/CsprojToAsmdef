c2d09665e5021810b3abb765f1f7fbe3b64d9c0b
Kurai András <k.andras9642@gmail.com>
2021-04-15 19:27:46 +0200

    Add sample internal dependency with serilog logging

---
+++ CsprojToAsmdef/Assets/SampleUnityDependency/CustomLogger.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+using Serilog.Sinks.Unity3D;
+using ILogger = Serilog.ILogger;
+
+namespace SampleUnityDependency
+{
+    public static class CustomLogger
+    {
+        private static readonly ILogger MyLogger = new LoggerConfiguration()
+            .WriteTo.Unity3D()
+            .CreateLogger();
+
+        // ReSharper disable once TemplateIsNotCompileTimeConstantProblem
+        public static void LogWarning(string message) => MyLogger.Warning(message);
+    }
+}
