cbf92f094d72996bd3f71ab6640c5be7ea2c9bae
Kurai András <k.andras9642@gmail.com>
2021-04-17 00:32:02 +0200

    create github release when pushing tags

--- CsprojToAsmdef.Build/NuGet.cs
+++ CsprojToAsmdef.Build/NuGet.cs
@@ -1,8 +1,11 @@
 ﻿using Nuke.Common;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
+using System.Text;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Nuke.Common.Tools.Git.GitTasks;
 using static System.IO.Directory;
@@ -14,6 +17,8 @@ partial class Build
 
     [Parameter("NuGet API key to use for authentication with the NuGet server")]
     readonly string? NugetApiKey;
+
+    [PathExecutable] readonly Tool Gh = default!;
 
     Target Pack => _ => _
         .DependsOn(BuildCli)
@@ -33,6 +38,14 @@ partial class Build
 
             Git($"tag {version}");
             Git($"push origin {version}");
+
+            var notes = File.ReadAllLines(Path.Combine(PackageDirectory, "CHANGELOG.md"))
+                .Skip(1)
+                .TakeUntil(string.IsNullOrWhiteSpace)
+                .Aggregate(new StringBuilder(), (sb, l) => sb.AppendLine(l))
+                .ToString();
+
+            Gh($"release create {version} -t {version} -n \"{notes}\"");
         });
 
     Target PushNuGet => _ => _
